f149dbe770271e513d558b849f9359b468dc4fc9
kevinldg <kevinloeding@outlook.de>
2025-03-07 03:37:47 +0100

    FRONTEND: Install React router, setup route for index page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,11 @@
+import {Route, Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage.tsx";
+
+
 export default function App() {
-    return <p>Hello World</p>;
+    return (
+        <Routes>
+            <Route path="/" element={<IndexPage/>} />
+        </Routes>
+    );
 }
