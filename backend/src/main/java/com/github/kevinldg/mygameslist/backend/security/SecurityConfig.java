7ba1fb1ff74b08bb93daff8571ae13fc547fdb3e
kevinldg <kevinloeding@outlook.de>
2025-03-26 11:19:39 +0100

    BACKEND: Disable CSRF only partially

--- backend/src/main/java/com/github/kevinldg/mygameslist/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/github/kevinldg/mygameslist/backend/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,10 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/auth/**"))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated());
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+        );
         return http.build();
     }
 
