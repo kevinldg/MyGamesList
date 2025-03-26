8c5a6e289f8e56d2f058a94a8af8eef172a53d0b
kevinldg <kevinloeding@outlook.de>
2025-03-26 11:31:48 +0100

    BACKEND: Improve partially disabling of CSRF

--- backend/src/main/java/com/github/kevinldg/mygameslist/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/github/kevinldg/mygameslist/backend/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.github.kevinldg.mygameslist.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +16,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/api/auth/**"))
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers(
+                                request -> HttpMethod.POST.name().equals(request.getMethod()) &&
+                                        ("/api/auth/login".equals(request.getRequestURI()) || "/api/auth/register".equals(request.getRequestURI()))
+                        ))
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-        );
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 
